af6583b79449d79b39624c9d66bd0d54daa2b420
attackq <rastpk@gmail.com>
2022-08-12 14:04:33 +0300

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="logo">
+                intouch
+            </div>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img
+                    src="https://image.winudf.com/v2/image/Y29tLnNnbS5iZWF1dGlmdWxiZWFjaHdhbGxwYXBlcl9zY3JlZW5zaG90c18wXzFjMTMxZGFj/screen-0.jpg?fakeurl=1&type=.webp"
+                    alt=""/>
+            </div>
+            <div>
+                description + ava
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
